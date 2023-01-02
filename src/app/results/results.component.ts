d77ca8e890fc8cdc43746daab5a949448921243e
hatim chifa <81763111+hatimc21@users.noreply.github.com>
2023-01-02 17:14:24 +0100

    certif

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
+  providers: [DatePipe]
 })
 export class ResultsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  score:any;
+  total:any;
+  name: any;
+  quizz_nom :any;
+  scorper:any;
+  todaydate : any;
+  constructor(private route: ActivatedRoute,private datePipe: DatePipe) { }
+
   ngOnInit(): void {
-  const score = this.route.snapshot.queryParamMap.get('score');
-  const total = this.route.snapshot.queryParamMap.get('total');
-  const name = this.route.snapshot.queryParamMap.get('name');
-  const quizz_nom = this.route.snapshot.queryParamMap.get('quizz_nom');
-  console.log( score);
-  console.log(total);
-  console.log(name);
-  console.log(quizz_nom);
+  this.score = this.route.snapshot.queryParamMap.get('score');
+  this.total = this.route.snapshot.queryParamMap.get('total');
+  this.name = this.route.snapshot.queryParamMap.get('name');
+  this.quizz_nom = this.route.snapshot.queryParamMap.get('quizz_nom');
+  this.scorper = (this.score*100)/this.total;
+  this.todaydate = this.datePipe.transform((new Date), 'dd/MM/yyyy');
+  console.log(this.todaydate);
+
   }
 
 }
